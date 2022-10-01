9d5d122d00bbc722e0ef108da81f74209807b083
Rohit kumar <112196544+0Rohit2022@users.noreply.github.com>
2022-10-01 21:42:39 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const imagecontainerEl = document.querySelector(".image-container");
+
+const prevEl = document.getElementById("prev")
+const nextEl = document.getElementById("next");
+
+let x = 0;
+let timer;
+
+
+prevEl.addEventListener("click", () => {
+    x += 45;
+    // console.log(x);
+    clearTimeout(timer);
+    updateGallery();
+});
+
+nextEl.addEventListener("click", () => {
+    x-= 45;
+    clearTimeout(timer);
+    updateGallery();
+
+})
+
+function updateGallery(){
+    imagecontainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`
+
+    
+   timer =  setTimeout(() => {
+        x -= 45;
+        updateGallery()
+       }, 5000);
+}
+updateGallery()
